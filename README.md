faaa88d7cf5115a255817edd2ebc842f1e1c55e2
Kirill Chernyy <kirill070@gmail.com>
2023-10-18 12:06:00 +0300

    Second step

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Kirill070/php-project-9/workflows/hexlet-check/badge.svg)](https://github.com/Kirill070/php-project-9/actions)<br>
 [![Page Analyzer](https://github.com/Kirill070/php-project-9/actions/workflows/my-check.yml/badge.svg)](https://github.com/Kirill070/php-project-9/actions/workflows/my-check.yml)<br>
+[Сервис Page Analyzer](https://page-analyzer-9r7p.onrender.com)<br>
--- public/index.php
+++ public/index.php
@@ -1,17 +1,20 @@
 <?php
 
-// Подключение автозагрузки через composer
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
-});
+    return $this->get('renderer')->render($response, 'index.phtml');
+})->setName('/');
+
 $app->run();
