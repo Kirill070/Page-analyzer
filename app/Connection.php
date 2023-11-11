d31d9e5c4354bb0ba677ef92c80efbaa8d9729b3
Kirill Chernyy <kirill070@gmail.com>
2023-11-11 21:23:37 +0300

    Sixth step

--- app/Connection.php
+++ app/Connection.php
@@ -18,22 +18,32 @@ final class Connection
             throw new \Exception("Error reading database url");
         }
 
-        $username = $databaseUrl['user'];
-        $password = $databaseUrl['pass'];
-        $host = $databaseUrl['host'];
-        $dbName = ltrim($databaseUrl['path'], '/');
+        // $username = $databaseUrl['user'];
+        // $password = $databaseUrl['pass'];
+        // $host = $databaseUrl['host'];
+        // $dbName = ltrim($databaseUrl['path'], '/');
 
-        $conStr = sprintf(
-            "pgsql:host=%s;dbname=%s;user=%s;password=%s",
-            $host,
-            $dbName,
-            $username,
-            $password
-        );
+        // $conStr = sprintf(
+        //     "pgsql:host=%s;dbname=%s;user=%s;password=%s",
+        //     $host,
+        //     $dbName,
+        //     $username,
+        //     $password
+        // );
 
-        $pdo = new \PDO($conStr);
-        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        // $pdo = new \PDO($conStr);
+        // $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
+
+        $host = isset($databaseUrl['host']) ? $databaseUrl['host'] : '';
+        $port = isset($databaseUrl['port']) ? $databaseUrl['port'] : '';
+        $dbname = isset($databaseUrl['path']) ? ltrim($databaseUrl['path'], '/') : '';
+        $username = isset($databaseUrl['user']) ? $databaseUrl['user'] : '';
+        $password = isset($databaseUrl['pass']) ? $databaseUrl['pass'] : '';
+
+        $conStr = "pgsql:host=$host;port=$port;dbname=$dbname";
+        $opt = array(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $pdo = new \PDO($conStr, $username, $password, $opt);
         return $pdo;
     }
 
